228ee6de020bacdc6f8d2b2732d986649107d135
hudenian <hudenian@163.com>
2021-12-16 10:52:21 +0800

    process java.lang.ClassCastException: org.springframework.core.io.buffer.DefaultDataBufferFactory error

--- cloud-gateway/src/main/java/com/huma/gateway/GateWayApplication.java
+++ cloud-gateway/src/main/java/com/huma/gateway/GateWayApplication.java
@@ -3,7 +3,10 @@ package com.huma.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+/**
+ * @author hudenian
+ * @date 2021/12/15
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GateWayApplication {
